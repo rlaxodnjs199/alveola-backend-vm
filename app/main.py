d8965e1ea489257ea04958a70e882ac00a954eba
Taewon Kim <rlaxodnjs199@gmail.com>
2021-10-06 09:54:03 -0500

    update config setting

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app import config
+
 
 app = FastAPI()
 
@@ -18,3 +20,12 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to my To Do list"}
+
+
+@app.get("/info")
+async def info():
+    return {
+        "raw": config.RAW_CT_PATH,
+        "deid": config.DEID_CT_PATH,
+        "vidaProcessed": config.VIDA_PROCESSED_CT_PATH,
+    }
