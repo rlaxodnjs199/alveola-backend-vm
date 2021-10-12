2ba1b14fae452df3896b82e1591cc2af70834451
Taewon Kim <rlaxodnjs199@gmail.com>
2021-10-12 10:04:37 -0500

    update db setting

--- app/db/pgsql/base_model.py
+++ app/db/pgsql/base_model.py
@@ -1,3 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import as_declarative
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import Column, Integer
 
-Base = declarative_base()
+
+@as_declarative()
+class Base:
+    id = Column(Integer, primary_key=True)
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
